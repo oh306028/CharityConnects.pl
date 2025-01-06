93a5df96b637183ce8f90289c9273c53ae2fd689
Oskar <hamerlao@gmail.com>
2025-01-06 14:19:36 +0100

    created exception handling middleware

---
+++ API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace API.Middlewares
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try {              
+               await next.Invoke(context);
+                     
+            }catch(Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync(ex.Message);
+            }
+
+
+        }
+    }
+}
