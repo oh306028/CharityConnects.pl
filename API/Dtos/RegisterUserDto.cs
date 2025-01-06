520027301c90449a6c220a6e3104f34ffd2e8040
Oskar <hamerlao@gmail.com>
2025-01-06 14:19:02 +0100

    created login aand register dtos

---
+++ API/Dtos/RegisterUserDto.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Dtos
+{
+    public class RegisterUserDto
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Password { get; set; }    
+
+        public int Age { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string Role { get; set; }
+
+        public int? OrganizationId { get; set; } 
+    }
+}
