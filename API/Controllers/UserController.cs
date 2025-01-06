4b2930dd780801cf4f2afdbf4e1812ca5a5f18ac
Oskar <hamerlao@gmail.com>
2025-01-06 14:18:31 +0100

    created user service and controller and register method

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using API.Dtos;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody] RegisterUserDto dto)
+        {
+            await _userService.RegisterUserAsync(dto);
+            return Created("api/accounts", null);
+        }
+
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginUserDto dto)    
+        {
+            await _userService.LoginUserAsync(dto);
+            return Ok();   
+        }
+
+    }
+}
---
+++ API/Services/UserService.cs
@@ -0,0 +1,96 @@
+﻿using API.Dtos;
+using API.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Services
+{
+    public interface IUserService
+    {
+        Task LoginUserAsync(LoginUserDto dto);
+        Task RegisterUserAsync(RegisterUserDto dto);    
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly CharityDbContext _dbContext;
+
+        public UserService(CharityDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+        public async Task RegisterUserAsync(RegisterUserDto dto)
+        {
+            Role role;
+            User newUser;
+
+
+            switch (dto.Role)
+            {
+                case "Beneficiary":
+                    role = Role.Beneficiary;
+
+                    newUser = new Beneficiary()
+                    {
+                        Email = dto.Email,
+                        FirstName = dto.FirstName,
+                        LastName = dto.LastName,
+                        Age = dto.Age,           
+                        DateOfBirth = dto.DateOfBirth
+                    };
+
+                    break;
+                case "Donor":
+                    role = Role.Donor;
+
+                    newUser = new Donor()   
+                    {
+                        Email = dto.Email,
+                        FirstName = dto.FirstName,
+                        LastName = dto.LastName,
+                        Age = dto.Age,
+                        DateOfBirth = dto.DateOfBirth
+                    };
+
+                    break;
+                case "Employee":
+                    role = Role.Employee;
+
+                    newUser = new Employee()
+                    {
+                        Email = dto.Email,
+                        FirstName = dto.FirstName,
+                        LastName = dto.LastName,
+                        Age = dto.Age,
+                        OrganizationId = dto.OrganizationId,
+                        DateOfBirth = dto.DateOfBirth
+                    };
+
+                    break;
+                default:
+                    throw new Exception("Invalid role");
+            }
+
+            var passwordHasher = new PasswordHasher<User>();
+            var hashedPassword = passwordHasher.HashPassword(newUser, dto.Password);
+            newUser.PasswordHash = hashedPassword;
+
+            _dbContext.Users.Add(newUser);
+            await _dbContext.SaveChangesAsync();
+
+
+        }   
+
+
+        public async Task LoginUserAsync(LoginUserDto dto)
+        {
+
+        }
+
+
+
+
+    }
+}
