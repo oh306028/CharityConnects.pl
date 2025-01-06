df895afdc355059f383f77637763712e250b498d
Oskar <hamerlao@gmail.com>
2025-01-06 14:18:46 +0100

    created validator for registerUserDTo

---
+++ API/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,47 @@
+﻿using API.Dtos;
+using FluentValidation;
+
+namespace API.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator()
+        {
+            RuleFor(e => e.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(53);
+
+            RuleFor(e => e.FirstName)
+               .NotEmpty()
+               .MaximumLength(20);
+
+            RuleFor(e => e.LastName)
+               .NotEmpty()
+               .MaximumLength(20);
+
+            RuleFor(e => e.Age)
+               .NotEmpty();
+
+            RuleFor(e => e.DateOfBirth)
+               .NotEmpty();
+
+            RuleFor(e => e.Role)
+               .NotEmpty();
+
+            RuleFor(x => x.OrganizationId).Custom((org, context) =>
+            {
+                if (context.InstanceToValidate.Role == "Employee" && org == null)
+                {
+                    context.AddFailure("OrganizationId must be provided.");
+                }
+            });
+
+            RuleFor(p => p.Password)
+                .NotEmpty();
+
+
+
+        }
+    }
+}
