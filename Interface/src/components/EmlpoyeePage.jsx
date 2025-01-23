74eaffa4af8114f2bf1e56a6edf8ca0c8c370bef
Oskar <hamerlao@gmail.com>
2025-01-23 19:20:33 +0100

    added component for employee page; navigation to project creation and managing the projects; no api call for projects yet

---
+++ Interface/src/components/EmlpoyeePage.jsx
@@ -0,0 +1,17 @@
+const EmployeePage = () => {
+  return (
+    <>
+      <div className="Container">
+        <p>Projekty</p>
+        <ul>
+          <li>project 1</li>
+          <li>project 2</li>
+        </ul>
+
+        <a href="employee/manage/create">Utwórz nowy projekt</a>
+      </div>
+    </>
+  );
+};
+
+export default EmployeePage;
