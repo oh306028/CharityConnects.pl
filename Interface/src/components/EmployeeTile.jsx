ffbdbf9b129f681c99f58bc9b40de6deca9baa76
Oskar <hamerlao@gmail.com>
2025-01-26 11:40:46 +0100

    refactored the tile; added props to handle data, deleting method on click

--- Interface/src/components/EmployeeTile.jsx
+++ Interface/src/components/EmployeeTile.jsx
@@ -1,6 +1,24 @@
 import styles from "../styles/EmployeeTile.module.css";
 
 const EmployeeTile = (props) => {
+  const handleDeleteEmployee = async () => {
+    try {
+      const response = await fetch(
+        `https://localhost:7292/api/employees/${props.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `bearer ${props.token}`,
+          },
+        }
+      );
+      if (response.ok) {
+        props.fetchEmployees();
+      }
+    } catch (error) {}
+  };
+
   return (
     <>
       <div className={styles.container}>
@@ -8,7 +26,9 @@ const EmployeeTile = (props) => {
           {props.firstName} {props.lastName}
         </h1>
         <p>{props.email}</p>
-        <button className={styles.deleteButton}>Zwolnij</button>
+        <button onClick={handleDeleteEmployee} className={styles.deleteButton}>
+          Zwolnij
+        </button>
       </div>
     </>
   );
