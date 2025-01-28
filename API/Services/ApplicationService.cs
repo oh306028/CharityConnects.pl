656353ef9f78d799e64ed7a85ae5433c7f030aae
Oskar <hamerlao@gmail.com>
2025-01-28 19:00:08 +0100

    refactored the service to store the data, apply and created new applicationService

---
+++ API/Services/ApplicationService.cs
@@ -0,0 +1,81 @@
+﻿using API.Dtos;
+using API.Exceptions;
+using API.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+    public interface IApplicationService
+    {
+        Task Apply(int projectId, IFormFile file);
+        Task<IEnumerable<ApplicationDto>> GetApplications(int projectId);
+    }
+
+    public class ApplicationService : IApplicationService
+    {
+        private readonly CharityDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly IUserContextService _userContextService;
+
+        public ApplicationService(CharityDbContext dbContext, IMapper mapper, IUserContextService userContextService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _userContextService = userContextService;
+        }
+
+        public async Task Apply(int projectId, IFormFile file)
+        {
+            if (file is null || file.Length == 0)
+                throw new BadHttpRequestException("File not found");
+
+            var beneficiaryId = _userContextService.UserId;
+            var beneficiary = _dbContext.Beneficiaries.FirstOrDefault(i => i.Id == beneficiaryId);
+            var project = _dbContext.CharityProjects
+                .Include(b => b.Beneficiaries)
+                .ThenInclude(b => b.Beneficiary)
+                .ThenInclude(a => a.Applications)   
+                .FirstOrDefault(i => i.Id == projectId);
+
+            if (project is null)
+                throw new NotFoundException("Project not found");
+
+
+            string fileContent;
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                fileContent = await reader.ReadToEndAsync();
+            }
+
+            var application = new Application()
+            {
+                Description = fileContent,
+                Beneficiary = beneficiary,
+                CharityProject = project,
+                IsAccepted = false
+                
+            };
+
+
+            _dbContext.Applications.Add(application);
+            await _dbContext.SaveChangesAsync();
+
+        }
+
+        public async Task<IEnumerable<ApplicationDto>> GetApplications(int projectId)
+        {
+            var applications = await _dbContext.Applications
+                .Include(b => b.Beneficiary)
+                .AsNoTracking()
+                .Where(i => i.CharityProjectId == projectId && (i.IsAccepted == false || i.IsAccepted == null)).ToListAsync();
+
+            var result = _mapper.Map<IEnumerable<ApplicationDto>>(applications);
+            return result;
+
+
+        }
+
+
+    }
+}
--- API/Services/CharityProjectService.cs
+++ API/Services/CharityProjectService.cs
@@ -10,6 +10,8 @@ namespace API.Services
     {
         Task<IEnumerable<CharityProjectDto>> GetAllProjectsAsync();
         Task<int> CreateCharityProjectAsync(CreateProjectDto dto);
+        Task Support(int projectId, int beneficiaryId);
+        Task<IEnumerable<CharityProjectDto>> GetOrganizationProjectsAsync();
     }
 
     public class CharityProjectService : ICharityProjectService
@@ -33,7 +35,22 @@ namespace API.Services
                .Include(d => d.Donors).ThenInclude(d => d.Donor)
                .Include(b => b.Beneficiaries).ThenInclude(d => d.Beneficiary)
                .Include(r => r.Requirements)
+               .Include(a => a.Applications).ThenInclude(b => b.Beneficiary)
                .AsNoTracking()
+               .ToListAsync();
+
+            var result = _mapper.Map<IEnumerable<CharityProjectDto>>(projects);
+            return result;
+        }
+
+        public async Task<IEnumerable<CharityProjectDto>> GetOrganizationProjectsAsync()    
+        {
+            var projects = await _dbContext.CharityProjects
+               .Include(d => d.Donors).ThenInclude(d => d.Donor)
+               .Include(b => b.Beneficiaries).ThenInclude(d => d.Beneficiary)
+               .Include(r => r.Requirements)
+               .AsNoTracking()
+               .Where(i => i.Employee.OrganizationId == _userContextService.UserId)
                .ToListAsync();
 
             var result = _mapper.Map<IEnumerable<CharityProjectDto>>(projects);
@@ -56,5 +73,40 @@ namespace API.Services
 
             return newProject.Id;
         }
+
+        public async Task Support(int projectId, int beneficiaryId)    
+        {
+            var projectToApply = _dbContext.CharityProjects
+                .Include(b => b.Beneficiaries)
+                .ThenInclude(b => b.Beneficiary)
+                .ThenInclude(a => a.Applications)
+                .AsNoTracking()
+                .FirstOrDefault(i => i.Id == projectId);
+                
+
+            if (projectToApply is null)
+                throw new NotFoundException("Project to apply not found");
+          
+            var application = _dbContext.Applications.FirstOrDefault(i => i.BeneficiaryId == beneficiaryId);
+
+            if (application is null)
+                throw new NotFoundException("Application not found");   
+
+            var beneficiary = _dbContext.Users.FirstOrDefault(i => i.Id == beneficiaryId);
+            if (beneficiary is null)
+                throw new NotFoundException("Beneficiary not found");
+
+            projectToApply.Beneficiaries
+                .Add(new ProjectBeneficiary() 
+                { Beneficiary = (Beneficiary)beneficiary, CharityProjectId = projectId });
+
+            application.IsAccepted = true;
+
+            await _dbContext.SaveChangesAsync();
+
+
+        }
+
+
     }
 }
