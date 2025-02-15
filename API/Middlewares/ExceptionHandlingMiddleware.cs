b878814b212395e06ddffa36a786fdcefe710489
Oskar <hamerlao@gmail.com>
2025-02-15 06:11:09 +0100

    added notFound exception handling block

--- API/Middlewares/ExceptionHandlingMiddleware.cs
+++ API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -1,15 +1,25 @@
 ﻿
 
+using API.Exceptions;
+
 namespace API.Middlewares
 {
     public class ExceptionHandlingMiddleware : IMiddleware
     {
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            try {              
-               await next.Invoke(context);
-                     
-            }catch(Exception ex)
+            try
+            {
+                await next.Invoke(context);
+
+
+            }
+            catch (NotFoundException notFound)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFound.Message);
+            }
+            catch (Exception ex)
             {
                 context.Response.StatusCode = 500;
                 await context.Response.WriteAsync(ex.Message);
